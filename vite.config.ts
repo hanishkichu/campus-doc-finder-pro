696e075b9bde334b26b0b71ae4bb54ca389e63bd
hanishkichu <150270997+hanishkichu@users.noreply.github.com>
2025-04-25 18:14:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "campus-doc-finder-pro",  // 👈 Add this for GitHub Pages deployment
   server: {
     host: "::",
-    port: 8080,
+    port: 8087,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
